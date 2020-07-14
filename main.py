667c842d335fc0769d247a3eb57c4b21656d507b
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-14 17:24:37 +0000

    LizvethM

---
+++ main.py
@@ -0,0 +1,15 @@
+a = "Lizveth Morales"
+b = "Ariana Grande"
+c = "Disneyland"
+d = "digitalart"
+e = "7/14/2020"
+
+print(a)
+
+print("My favorite artist is"+ b)
+
+print("I would like a vacation to"+c)
+
+print("My hobby is doing"+d)
+
+print("Today is "+ e)
